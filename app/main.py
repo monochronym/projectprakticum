d5d2bbd545cf1defb85239a13163804de545e6a5
Alexandr Borisov <77160305+monochronym@users.noreply.github.com>
2025-06-10 21:04:11 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,5 +21,5 @@ async def hello():
 
 app.include_router(api)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
