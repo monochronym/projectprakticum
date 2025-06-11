c66f8a2a94c714dcf3cfc8dd2c6cec9b06ebc53c
monochronym <paiy1806@gmail.com>
2025-06-11 16:06:58 +0500

    Putting files

--- app/routers/basket.py
+++ app/routers/basket.py
@@ -47,5 +47,5 @@ async def delete_basketItems(basketItem_id:str):
     return BasketItemDAO.delete( False,id=basketItem_id)
 
 @basketItemsRouter.delete("/basketItem")
-async def delete_basketItems_by_goodId(good_id:str):
-    return BasketItemDAO.delete( False,goodId=good_id)
+async def delete_basketItems_by_goodId(good_id:str, basket_id:str):
+    return BasketItemDAO.delete( False,goodId=good_id, basket_id=basket_id)
